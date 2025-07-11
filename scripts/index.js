a2ef4caa0a94fadf3fadcdd19101d471d4aacccf
Broninn <bruno-hs@outlook.com>
2025-07-11 13:51:36 -0300

    finish button

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,37 @@ const askButton = document.getElementById("askButton");
 const aiResponse = document.getElementById("aiResponse");
 const form = document.getElementById("form");
 
-form.addEventListener("submit", (e) => {
+const askAI = async (question, game, api) => {
+  const model = "gemini-2.0-flash"
+  const baseURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${api}`;
+  //28:25 video
+};
+
+const sendForm = (e) => {
   e.preventDefault();
-  console.log(questionInput.value);
-});
+  const apiKey = apiKeyInput.value;
+  const game = gameSelect.value;
+  const question = questionInput.value;
+  console.log({ apiKey, game, question });
+
+  if (apiKey == "" || game == "" || question == "") {
+    alert("Please fill in all fields");
+    return;
+  }
+  askButton.disabled = true;
+  askButton.textContent = "Perguntando...";
+  askButton.classList.add("loading");
+
+  try {
+    // Perguntar para GEMINI
+    askAI(question, game, apiKey);
+  } catch (error) {
+    console.log("Erro", error);
+  } finally {
+    askButton.disabled = false;
+    askButton.textContent = "Perguntar";
+    askButton.classList.remove("loading");
+  }
+};
+
+form.addEventListener("submit", sendForm);
